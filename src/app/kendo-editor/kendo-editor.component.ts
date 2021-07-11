c44dc30d8b70fb6d88b2e7611099515fc9863836
gnoose <jieli3838@gmail.com>
2021-07-11 12:44:01 +0300

    feat: add fontSize, change font Size&Family

--- src/app/kendo-editor/kendo-editor.component.ts
+++ src/app/kendo-editor/kendo-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ClipboardService } from 'ngx-clipboard';
+import { FontSizeItem } from '@progress/kendo-angular-editor/dist/es2015/common/font-size-item.interface';
 
 @Component({
   selector: 'app-kendo-editor',
@@ -27,6 +28,41 @@ export class KendoEditorComponent implements OnInit {
         </ul>
     `;
 
+  public fontList: FontSizeItem[] = [
+    {
+      size: 8,
+      text: '8px'
+    },
+    {
+      size: 10,
+      text: '10px'
+    },
+    {
+      size: 12,
+      text: '12px'
+    },
+    {
+      size: 14,
+      text: '14px'
+    },
+    {
+      size: 18,
+      text: '18px'
+    },
+    {
+      size: 24,
+      text: '24px'
+    },
+    {
+      size: 36,
+      text: '36px'
+    },
+    {
+      size: 54,
+      text: '54px'
+    },
+  ];
+
   constructor(
     private clipboardApi: ClipboardService
   ) { }
@@ -40,6 +76,27 @@ export class KendoEditorComponent implements OnInit {
 
   valueChanged(event: any) {
     this.clipBoardContent = event;
+  }
+
+  addFont() {
+    const newFontSize = this.fontList[this.fontList.length-1].size + 18;
+    this.fontList.push({
+      size: newFontSize,
+      text: `${newFontSize}px`
+    });
+  }
+
+  setFontSize(fontElement: any) {
+    fontElement.value = 12;
+  }
+
+  setFontFamily(fontElement: any) {
+    fontElement.value = 'Arial,"Helvetica Neue",Helvetica,sans-serif';
+  }
+
+  fontFamilyChanged(event: any, obj: any) {
+    console.log('obj = ', obj);
+    console.log('event = ', event);
   }
 
 }
